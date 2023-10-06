e9abfc03b068e282749fc8721f2ce75217ff40bb
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2023-10-06 13:32:20 +0300

    Add files via upload

---
+++ bolum2-odev3/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace bolum2_odev3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGır = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGır
+            // 
+            this.btnGır.Location = new System.Drawing.Point(28, 50);
+            this.btnGır.Name = "btnGır";
+            this.btnGır.Size = new System.Drawing.Size(75, 23);
+            this.btnGır.TabIndex = 0;
+            this.btnGır.Text = "b";
+            this.btnGır.UseVisualStyleBackColor = true;
+            this.btnGır.Click += new System.EventHandler(this.btnGır_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnGır);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGır;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
